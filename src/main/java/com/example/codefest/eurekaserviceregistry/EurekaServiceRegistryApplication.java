ad427d26fc466335d247fcbbed75efe44f466abc
veenanc01 <veena.chandrappa@accenture.com>
2023-01-26 19:42:07 +0530

    updated for war deployment

--- src/main/java/com/example/codefest/eurekaserviceregistry/EurekaServiceRegistryApplication.java
+++ src/main/java/com/example/codefest/eurekaserviceregistry/EurekaServiceRegistryApplication.java
@@ -2,14 +2,21 @@ package com.example.codefest.eurekaserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceRegistryApplication {
+public class EurekaServiceRegistryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+		return builder.sources(EurekaServiceRegistryApplication.class);
+	}
 }
