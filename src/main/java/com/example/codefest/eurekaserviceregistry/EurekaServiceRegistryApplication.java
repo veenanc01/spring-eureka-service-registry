932cf9dc28ad4d50d8967a5f059960ec49ee2244
veenanc01 <veena.chandrappa@accenture.com>
2023-01-30 11:08:05 +0530

    updated pom file and java file

--- src/main/java/com/example/codefest/eurekaserviceregistry/EurekaServiceRegistryApplication.java
+++ src/main/java/com/example/codefest/eurekaserviceregistry/EurekaServiceRegistryApplication.java
@@ -8,15 +8,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceRegistryApplication extends SpringBootServletInitializer {
+public class EurekaServiceRegistryApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 
-		return builder.sources(EurekaServiceRegistryApplication.class);
-	}
 }
