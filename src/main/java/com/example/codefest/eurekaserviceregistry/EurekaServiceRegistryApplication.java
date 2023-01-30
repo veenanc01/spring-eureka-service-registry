36df75797fcadfa23a59e12195d09cd555ccd297
veenanc01 <veena.chandrappa@accenture.com>
2023-01-30 19:39:35 +0530

    updated

--- src/main/java/com/example/codefest/eurekaserviceregistry/EurekaServiceRegistryApplication.java
+++ src/main/java/com/example/codefest/eurekaserviceregistry/EurekaServiceRegistryApplication.java
@@ -2,8 +2,6 @@ package com.example.codefest.eurekaserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
